64cb0d466b6aa5851ff8a372ca44d313edca6d6b
Harilegawa <120030681+WenaHarle@users.noreply.github.com>
2023-05-05 14:38:46 +0700

    Update Videor.py

--- Videor.py
+++ Videor.py
@@ -1,7 +1,5 @@
 import cv2
 
-
-
 # Create the video capture object and set the frame size
 cap = cv2.VideoCapture(0)
 cap2 = cv2.VideoCapture(2)
@@ -14,25 +12,25 @@ fps = 30
 
 Atas = cv2.VideoWriter('Depan.mp4', codec, fps, frame_size)
 Kanan = cv2.VideoWriter('Kanan.mp4', codec, fps, frame_size)
-Kiri  = cv2.VideoWriter('Kiri.mp4', codec, fps, frame_size)
+Kiri = cv2.VideoWriter('Kiri.mp4', codec, fps, frame_size)
 
 # Start the video capture and writing loop
 while True:
     ret, frame = cap.read()
     ret2, frame2 = cap2.read()
     ret3, frame3 = cap3.read()
-    
+
     # checking the capture menthod is succesfull
-    if ret AND ret2 AND ret3:
+    if ret and ret2 and ret3:
 
         Atas.write(frame)
         Kanan.write(frame2)
         Kiri.write(frame3)
-        
+
         cv2.imshow('Atas', frame)
         cv2.imshow('Kanan', frame2)
         cv2.imshow('Kiri', frame3)
-        
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
@@ -40,5 +38,9 @@ while True:
 
 # Release the video capture and writer, and close the window
 cap.release()
-Depan.release()
+Atas.release()
+cap2.release()
+Kanan.release()
+cap3.release()
+Kiri.release()
 cv2.destroyAllWindows()
