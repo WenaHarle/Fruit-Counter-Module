78eeb26c1b12dc5cb5e7c4d683fa132fe556a405
Harilegawa <120030681+WenaHarle@users.noreply.github.com>
2023-05-07 14:36:25 +0700

    Update Videor.py

--- Videor.py
+++ Videor.py
@@ -1,35 +1,49 @@
 import cv2
 
 # Create the video capture object and set the frame size
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
+
 cap2 = cv2.VideoCapture(2)
-cap3 = cv2.VideoCapture(4)
+
+
+# Set the capture resolution
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+cap2.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap2.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
 
 # Create the video writer object with a compatible MP4 codec
-frame_size = (640, 480)
+frame_size = (1280, 720)
 codec = cv2.VideoWriter_fourcc(*'mp4v')
-fps = 30
+fps = 10
 
-Atas = cv2.VideoWriter('Depan.mp4', codec, fps, frame_size)
-Kanan = cv2.VideoWriter('Kanan.mp4', codec, fps, frame_size)
-Kiri = cv2.VideoWriter('Kiri.mp4', codec, fps, frame_size)
+Camera1 = cv2.VideoWriter('Camera1.mp4', codec, fps, frame_size)
+
+Camera2 = cv2.VideoWriter('Camera2.mp4', codec, fps, frame_size)
+
+
 
 # Start the video capture and writing loop
 while True:
     ret, frame = cap.read()
+
     ret2, frame2 = cap2.read()
-    ret3, frame3 = cap3.read()
+
 
     # checking the capture menthod is succesfull
-    if ret and ret2 and ret3:
+    if ret  and ret2 :
+        
+        #Write Video File
+        Camera1.write(frame)
+        Camera2.write(frame2)
+   
+        #Show Video In Camera
+        cv2.imshow('Camera1', frame)
+        cv2.imshow('Camera2', frame2)
 
-        Atas.write(frame)
-        Kanan.write(frame2)
-        Kiri.write(frame3)
 
-        cv2.imshow('Atas', frame)
-        cv2.imshow('Kanan', frame2)
-        cv2.imshow('Kiri', frame3)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -38,9 +52,9 @@ while True:
 
 # Release the video capture and writer, and close the window
 cap.release()
-Atas.release()
+Camera1.release()
 cap2.release()
-Kanan.release()
-cap3.release()
-Kiri.release()
+Camera2.release()
+
+
 cv2.destroyAllWindows()
